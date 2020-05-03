458315c44cac8817b4b8fad0284f185e84e058b7
evandrogouveia <vando_gouveia@hotmail.com>
2020-05-03 10:17:11 -0300

    Auto-generated commit

--- src/app/shared/models/cadastroservico.model.ts
+++
@@ -1,6 +0,0 @@
-export class Cadastroservico {
-    id?: string;
-    nomeservico: string;
-    valorservico: string;
-    imageUrl: string;
-}
--- src/app/shared/models/user.ts
+++
@@ -1,8 +0,0 @@
-export interface Usuario {
-    nome: string;
-    sobrenome: string;
-    telefone: string;
-    email: string;
-    password?: string;
-    id?: string;
-}
