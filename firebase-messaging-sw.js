f1af1be3ba74dd56f4cdfd884c122075724967cf
evandrogouveia <vando_gouveia@hotmail.com>
2020-05-13 21:13:29 -0300

    Auto-generated commit

--- firebase-messaging-sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-messaging.js');
-
-firebase.initializeApp({
-    'messagingSenderId': '1026898851432'
-});
-
-const messaging = firebase.messaging();
