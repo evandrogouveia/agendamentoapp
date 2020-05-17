c1ff413f3df67a7b838e1d2d4b37a23e628b050a
evandrogouveia <vando_gouveia@hotmail.com>
2020-05-17 17:54:04 -0300

    busca

--- src/app/cliente/index/index.component.ts
+++ src/app/cliente/index/index.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { SidebarService } from 'src/app/shared/services/sidebar.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { trigger, state, style, animate, transition } from '@angular/animations';
-
-
+import { BsModalService, ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-index',
@@ -17,15 +15,23 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
     ]),
   ]
 })
+
 export class IndexComponent implements OnInit {
-
-
+  @ViewChild('childModal') childModal: ModalDirective;
+  @ViewChild('busca') busca: any;
   isDropdown = false;
 
-  constructor(private sidebarService: SidebarService) { }
+  constructor() { }
 
   ngOnInit() {
-   
+
+  }
+
+  openModal(): void {
+    this.childModal.show();
+    setTimeout(() => {
+      this.busca.nativeElement.focus();
+    }, 500);
   }
 
   onOpenChange(data: boolean): void {
