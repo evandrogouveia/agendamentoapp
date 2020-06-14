ca87fa002699cecf96aa899970acc4ff9d0f73c9
evandrogouveia <vando_gouveia@hotmail.com>
2020-06-14 10:11:18 -0300

    adicionar mapbox

--- src/app/cliente/sucesso/sucesso.component.ts
+++ src/app/cliente/sucesso/sucesso.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-sucesso',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sucesso.component.css']
 })
 export class SucessoComponent implements OnInit {
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 37.75;
+  lng = -122.41;
+  initialPosition: any = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.startMap();
+
   }
+
+  startMap() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+      this.initialPosition = position.coords;
+      this.map.flyTo({
+        center: [this.lng, this.lat],
+      });
+      new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat({ lng: this.lng, lat: this.lat })
+        .setHTML('<div class="popup-map">' +
+                    '<img src="assets/img/logo-login.png"> ' +
+                    '<p>Rua Cardoso José Martins Sobrinho, 785/ RJ- Realengo</p>' +
+                '</div>'
+        )
+        .addTo(this.map);
+    });
+
+    (mapboxgl as typeof mapboxgl).accessToken = environment.mapbox.accessToken;
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 15,
+      center: [this.lng, this.lat],
+
+    });
+
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+  }
+
+
+
+
 
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,6 +12,9 @@ export const environment = {
     storageBucket: 'agendamento-f8fb6.appspot.com',
     messagingSenderId: '1026898851432',
     appId: '1:1026898851432:web:428a48906ea748ce'
+  },
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiZXZhbmRyb2dvdXZlaWEiLCJhIjoiY2tiZWN4cnVpMGtibjJvbndzYXN2dWZ3ZyJ9.YNZIDQr1MED6VTW9_dQkEA'
   }
 };
 
