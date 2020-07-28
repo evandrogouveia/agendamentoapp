9f1a7291b50d7bbcac0ced554bbe6f7d5412755e
evandrogouveia <vando_gouveia@hotmail.com>
2020-07-28 16:20:03 -0300

    ajustes perfil

--- src/app/cliente/perfil-cliente/perfil-cliente.component.ts
+++ src/app/cliente/perfil-cliente/perfil-cliente.component.ts
@@ -3,6 +3,8 @@ import { SidebarService } from 'src/app/shared/services/sidebar.service';
 import { AuthService } from 'src/app/login/auth/auth.service';
 import { Observable } from 'rxjs';
 import { Usuario } from 'src/app/shared/models/user';
+import { Agendamento } from 'src/app/shared/models/agendamento.model';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-perfil-cliente',
@@ -11,13 +13,19 @@ import { Usuario } from 'src/app/shared/models/user';
 })
 export class PerfilClienteComponent implements OnInit {
   usuario$: Observable<Usuario>;
+  agendamento$: Observable<Agendamento[]>;
   expanded;
   
-  constructor(private authService: AuthService, private sidebarService: SidebarService) { 
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService, 
+    private sidebarService: SidebarService
+    ) { 
     this.usuario$ = this.authService.getUser();
   }
 
   ngOnInit() {
+    this.agendamento$ = this.apiService.getAgendamentos();
    this.sidebarService.obterToggle().subscribe(data => {
     this.expanded = data;
    });
